0804d080d72eb542d6383a19646f20f1938a7349
DorianBoel <dorian.boel@free.fr>
2023-08-22 23:59:40 +0200

    add app base layout; modify app style; add bootstrap & fontawesome

---
+++ src/components/MainFooter.tsx
@@ -0,0 +1,14 @@
+const MainFooter = () => {
+    return (
+        <footer className="main-footer bg-light-subtle py-3">
+            <div className="small text-center">
+                <span className="fst-italic">
+                    2023 &copy; DorianBoel
+                </span>
+            </div>
+        </footer>
+
+    );
+}
+
+export default MainFooter;
---
+++ src/components/MainHeader.tsx
@@ -0,0 +1,20 @@
+import { Container, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../resources/logo.svg";
+
+const MainHeader = () => {
+    return (
+        <Navbar className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand>
+                    <Link to="/" className="text-black text-decoration-none">
+                        <img src={ logo } className="app-logo mb-1" alt="logo" />
+                        TP Memo
+                    </Link>
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default MainHeader;
